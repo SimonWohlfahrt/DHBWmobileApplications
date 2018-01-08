c127375de8112d5691e2cd5dfbed1228c43b3c11
Simon <simonwohlfahrt@live.de>
2018-01-08 23:20:59 +0100

    clean

--- Geschenke/app/src/main/java/com/example/weitlos/geschenke/MainActivity.java
+++ Geschenke/app/src/main/java/com/example/weitlos/geschenke/MainActivity.java
@@ -20,22 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void setIdle() {
-        l.setBackgroundColor(Color.parseColor("#ffffff"));
-    }
-
-    private void setSetting() {
-
-    }
-
-    private void setwaiting() {
-        l.setBackgroundColor(Color.parseColor("#BB5500"));
-    }
-
-    private void setWelcome() {
-        l.setBackgroundColor(Color.parseColor("#00BB22"));
-    }
-
     public void setup(View sender) {
         Intent i = new Intent(this, setupActivity.class);
         startActivity(i);
